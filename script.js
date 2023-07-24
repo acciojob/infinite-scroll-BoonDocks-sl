d52cb1bb328fc3633226a05ff23bcf710b517a1a
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-07-24 09:38:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
 
+//your code here!
+const list = document.getElementById('infi-list');
+
+list.addEventListener('scroll', function() {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems();
+  }
+});
+
+// Function to add more list items
+function addItems() {
+  // Number of new items to add
+  const itemsToAdd = 2;
+
+  // Generate new list items and append them to the list
+  for (let i = 1; i <= itemsToAdd; i++) {
+    const newItem = document.createElement('li');
+    newItem.textContent = 'Item ' + (list.childElementCount + i);
+    list.appendChild(newItem);
+  }
+}
